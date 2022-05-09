d8119827d2359cae609647a9484a412c74b3966a
Sondos Atef <sondos.ahmed031@gmail.com>
2022-05-09 21:41:45 +0200

    added github pages script

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
+  // publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  module.exports = {
+    publicPath: '/toys-shop/'
+  }
